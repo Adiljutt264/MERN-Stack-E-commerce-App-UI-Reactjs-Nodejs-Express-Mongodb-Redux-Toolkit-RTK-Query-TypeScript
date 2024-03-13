4a537be6e83a547c8443749eb0f9b80f846dd192
adil.javed <adil.javed@codup.co>
2024-03-13 15:29:22 +0500

    added cart.tsx

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,8 +1,44 @@
-
+import { useState, useEffect } from "react";
+import { VscError } from "react-icons/vsc";
+const cartItems = [];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 250;
+const discount = 400;
+const total = tax + shippingCharges + subtotal;
 
 const cart = () => {
+  const [couponCode, setCouponCode ] = useState<string>("");
+  const [ isvalidcouponCode, setIsValidCouponCode ] = useState<boolean>();
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if(Math.random()>0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false)
+    }, 1000);
+  
+    return () => {
+      clearTimeout(timeOutId)
+    }
+  }, [couponCode])
+  
   return (
-    <div>cart</div>
+    <div className="cart">
+      <main>
+      </main>
+      <aside>
+        <p>{`Subtotal: ${subtotal}`}</p>
+        <p>{`Shipping Charges: ${shippingCharges}`}</p>
+        <p>{`Tax: ${tax}`}</p>
+        <p>Discount: <em> {`- Discount ${discount}`}</em></p>
+        <p>Total <b>{total}</b></p>
+        <input type="text" value={couponCode} placeholder="Coupon code" onChange={(e) => setCouponCode(e.target.value)} />
+        {
+          couponCode && (
+            isvalidcouponCode ? ( <span className="green">{`- Discount ${discount}`} off using the <code>{couponCode}</code></span> ) : (<span className="red">Invalid Coupon <VscError /></span>)
+          )
+        }
+      </aside>
+    </div>
   )
 }
 
